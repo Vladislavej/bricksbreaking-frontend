1e3a74e67acc82f8532145521e62fe05451724dc
Vlados <vladislav.mocilenko@student.tuke.sk>
2024-04-24 21:48:44 +0200

    Init + CommentsComponent.js/ScoresComponent.js

---
+++ src/components/CommentsComponent.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import axios from 'axios';
+
+const COMMENTS_API_REST_URL = "http://localhost:8080/api/comment/bricksbreaking";
+
+export default class CommentsComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get(COMMENTS_API_REST_URL)
+            .then(response => {
+                this.setState({ comments: response.data });
+            })
+            .catch(error => {
+                console.error('Error fetching comments:', error);
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 className="text-center">Comments</h2>
+                <table className="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>Player</th>
+                        <th>Comment</th>
+                        <th>Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.comments.map(comment =>
+                        <tr key={comment.id}>
+                            <td>{comment.player}</td>
+                            <td>{comment.comment}</td>
+                            <td>{comment.commentedOn}</td>
+                        </tr>
+                    )}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
---
+++ src/components/ScoresComponent.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import axios from 'axios';
+
+const SCORES_API_REST_URL = "http://localhost:8080/api/score/bricksbreaking";
+
+export default class ScoresComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            scores: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get(SCORES_API_REST_URL)
+            .then(response => {
+                this.setState({ scores: response.data });
+            })
+            .catch(error => {
+                console.error('Error fetching scores:', error);
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 className="text-center">Scores</h2>
+                <table className="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>Player</th>
+                        <th>Points</th>
+                        <th>Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.scores.map(score =>
+                        <tr key={score.id}>
+                            <td>{score.player}</td>
+                            <td>{score.points}</td>
+                            <td>{score.playedOn}</td>
+                        </tr>
+                    )}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
